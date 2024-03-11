88cc76cd11520434239fa368c172052f65203411
arfan21 <arfan.jadul10@gmail.com>
2024-03-11 14:05:53 +0700

    add options in middleware timeout

--- pkg/middleware/timeout.go
+++ pkg/middleware/timeout.go
@@ -7,9 +7,41 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func Timeout(duration time.Duration) fiber.Handler {
+type OptFunc func(*Config)
+
+var defaultConfig = Config{
+	duration: 30 * time.Second,
+}
+
+type Config struct {
+	duration     time.Duration
+	excludePaths []string
+}
+
+func WithExcludePaths(paths ...string) OptFunc {
+	return func(c *Config) {
+		c.excludePaths = append(c.excludePaths, paths...)
+	}
+}
+
+func Timeout(duration time.Duration, opt ...OptFunc) fiber.Handler {
+	config := defaultConfig
+
+	if duration > 0 {
+		config.duration = duration
+	}
+
+	for _, o := range opt {
+		o(&config)
+	}
+
 	return func(c *fiber.Ctx) error {
-		ctx, cancel := context.WithTimeout(c.Context(), duration)
+		for _, path := range config.excludePaths {
+			if c.Path() == path {
+				return c.Next()
+			}
+		}
+		ctx, cancel := context.WithTimeout(c.Context(), config.duration)
 		defer cancel()
 		c.SetUserContext(ctx)
 		return c.Next()
